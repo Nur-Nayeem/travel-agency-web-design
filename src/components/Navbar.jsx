5bf49f1ee9bc08dfd0c4989d2af7845f4e57f702
nur-nayeem <nurnayem768@gmail.com>
2025-08-12 01:05:18 +0600

    make responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from "../assets/jadoo.jpg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     return (
-        <div className='flex justify-between items-center goole-font-sans'>
-            <div className='h-10'>
+        <div className='flex justify-between items-center goole-font-sans mb-10'>
+            <div className='lg:h-10 h-7'>
                 <img className='h-full w-full' src={logo} alt="" />
             </div>
-            <div className='flex gap-20'>
+            <div className='lg:hidden block'>
+                <FontAwesomeIcon icon={faEllipsisVertical} size='2x' />
+            </div>
+            <div className='lg:flex gap-20 hidden'>
                 <ul className='flex gap-12 my-11 text-lg font-medium cursor-pointer'>
                     <li>
                         <Link to={"/"}>Desitnations</Link>
