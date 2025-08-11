5dabbbe0af32947b081593630661d5789f86138c
nur-nayeem <nurnayem768@gmail.com>
2025-08-12 01:05:43 +0600

    fix some issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import { HashRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
       <HashRouter>
 
         <Routes>
-          <Route path='/' element={<Navbar />} />
+          <Route path='/' element={<Home />} />
           {/* <Route path='/' element={<Navbar />} />
           <Route path='/' element={<Navbar />} />
           <Route path='/' element={<Navbar />} /> */}
