8faf392f2b957f0470e12575f2cf751d9d92898d
nur-nayeem <nurnayem768@gmail.com>
2025-08-12 00:07:30 +0600

    added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from "../assets/jadoo.jpg"
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between items-center goole-font-sans'>
+            <div className='h-10'>
+                <img className='h-full w-full' src={logo} alt="" />
+            </div>
+            <div className='flex gap-20'>
+                <ul className='flex gap-12 my-11 text-lg font-medium'>
+                    <li>
+                        <Link to={"/"}>Desitnations</Link>
+                    </li>
+                    <li>
+                        <Link to={"/hotels"}>Hotels</Link>
+                    </li>
+                    <li>
+                        <Link to={"/flights"}>Flights</Link>
+                    </li>
+                    <li>
+                        <Link to={"/bookings"}>Bookings</Link>
+                    </li>
+                </ul>
+                <div className='flex justify-between gap-12 items-center text-lg font-medium'>
+                    <button>Login</button>
+                    <button>Sign up</button>
+                    <div>
+                        <p>EN <span>^</span></p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
